26df1e659a4bb030e7b44aef0445e220ba43f3d3
francielisabreu <contato.theframboesa@gmail.com>
2020-04-11 19:50:42 -0300

    commit cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,84 +1,38 @@
 const cipher = {
-  encode: function (deslocamento, mensagem) {
-    //variavél que irá receber o resultado
-    let resultado = "" ;
-    let formula;
-    for (let i=0; mensagem.length >i; i++) {
-      //confere se os caracteres recebidos são maiúsculos
-      if (mensagem.charCodeAt(i) >=65 && mensagem.charCodeAt(i) <=90) {
-        //confere se o offset é positivo e codifica a mensagem 
-        if (deslocamento >=0) {
-          formula = ((mensagem.charCodeAt(i) -65 + deslocamento)% 26)+65;
-          resultado += String.fromCharCode(formula);
-        } 
-        //confere se o offset é negativo e codifica a mensagem
-        else {
-          formula = ((mensagem.charCodeAt(i) -90 + deslocamento)% 26)+90;
-          resultado += String.fromCharCode(formula);
-        }
+  encode: function (offset, mensagem) {
+    let resultEnt = "";
+    // loop que verifica cada caractere no texto
+    for (let i = 0; i < mensagem.length; i++) {
+      let criptado = "";
+      let newEntr = 0;
+      let textEntr = mensagem[i].charCodeAt();
+
+      if (90 >= textEntr && textEntr >= 65) {
+        newEntr = (textEntr - 65 + offset) % 26 + 65;
+        criptado = String.fromCharCode(newEntr);
+
       }
-      //confere se os caracteres recebidos são minúsculos
-      else if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122) {
-        //confere se o offset é positivo e codifica a mensagem
-        if (deslocamento >=0) {
-          formula = ((mensagem.charCodeAt(i) -97 + deslocamento)% 26)+97;
-          resultado += String.fromCharCode(formula);
-        } 
-        else {
-          //se o offset for negativo ele codifica os caracteres 
-          formula = ((mensagem.charCodeAt(i) -122 + deslocamento)% 26)+122;
-          resultado += String.fromCharCode(formula);
-        }
-      }
-      //se for símbolo não há alteração 
-      else {
-        formula = (mensagem.charCodeAt(i));
-        resultado += String.fromCharCode(formula);
-      }
+      resultEnt = resultEnt + criptado;
     }
-    //resultado final da mensagem
-    return resultado;
+
+    return resultEnt;
   },
-  //Função com parâmetros de deslocamento e mensagem conforme as variáveis do arquivo index.js
-  decode: function (deslocamento, mensagem) {
-    //variavél que irá receber o resultado
-    let decoderesult = "" ;
-    let formula;
-    for (let i=0; mensagem.length >i; i++) {
-      //confere se os caracteres recebidos são maiúsculos 
-      if (mensagem.charCodeAt(i) >=65 && mensagem.charCodeAt(i) <=90) {
-        if (deslocamento >=0) {
-          //confere se o offset é positivo e decodifica a mensagem
-          formula = ((mensagem.charCodeAt(i) - 90 - deslocamento)% 26)+90;
-          decoderesult += String.fromCharCode(formula);
-        } 
-        //confere se o offset é negativo e decodifica a mensagem
-        else {
-          formula = ((mensagem.charCodeAt(i) - 65 - deslocamento)% 26)+65;
-          decoderesult += String.fromCharCode(formula);
-        }
+
+  decode: function (offset, mensagem) {
+    let resultEnt = "";
+
+    for (let i = 0; i < mensagem.length; i++) {
+      let criptado = "";
+      let newEntr = 0;
+      let textEntr = mensagem[i].charCodeAt();
+
+      if (90 >= textEntr && textEntr >= 65) {
+        newEntr = (textEntr + 65 - offset) % 26 + 65;
+        criptado = String.fromCharCode(newEntr);
       }
-      //confere se os caracteres recebidos são minúsculos
-      else if (mensagem.charCodeAt(i) >=97 && mensagem.charCodeAt(i) <=122) {
-        if (deslocamento >=0) {
-          //confere se o offset é positivo e decodifica a mensagem
-          formula = ((mensagem.charCodeAt(i) - 122 - deslocamento)% 26)+122;
-          decoderesult += String.fromCharCode(formula);
-        } 
-        //confere se o offset é negativo e decodifica a mensagem
-        else {
-          formula = ((mensagem.charCodeAt(i) - 97 - deslocamento)% 26)+97;
-          decoderesult += String.fromCharCode(formula);
-        }
-      }
-      //se for símbolo não há alteração 
-      else {
-        formula = (mensagem.charCodeAt(i));
-        decoderesult += String.fromCharCode(formula);
-      }
+      resultEnt = resultEnt + criptado;
     }
-    //resultado final da decodificação
-    return decoderesult;
+    return resultEnt;
   }
 };
 
