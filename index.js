7871f63ee1bbb752ed4ae46685b78de002ada47f
francielisabreu <contato.theframboesa@gmail.com>
2020-04-12 20:52:52 -0300

    Updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import cipher from './cipher.js'
 
 
 let cifrar = document.getElementById("cifrar")
-let descifrar = document.getElementById("descifrar")
+let decifrar = document.getElementById("decifrar")
 let strat = document.getElementById("start")
 let voltar = document.getElementById("voltar")
 let page1 = document.getElementById("page01")
@@ -16,10 +16,12 @@ strat.onclick = function(){
     page1.style.display = "none"
     page2.style.display = "flex"
 }
+
 voltar.onclick = function(){
     page1.style.display = "flex"
     page2.style.display = "none"
 }
+
 cifrar.onclick = function(){
     if (boxMensagem.value == "" || offset.value == "") {
         alert("Verifique se preencheu o campo mensagem e o valor de deslocamento");
@@ -29,7 +31,8 @@ cifrar.onclick = function(){
             result.style.display = "block"        
     }
 }
-descifrar.onclick = function(){
+
+decifrar.onclick = function(){
     if (boxMensagem.value == "" || offset.value == "") {
         alert("Verifique se preencheu o campo mensagem e o valor de deslocamento");        
 
