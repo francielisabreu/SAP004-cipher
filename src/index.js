ce9d472dc8c706f700439e8f9baa6d7d7f49ea32
francielisabreu <contato.theframboesa@gmail.com>
2020-04-12 00:21:04 -0300

    update botoes start e voltar

--- src/index.js
+++ src/index.js
@@ -8,9 +8,39 @@ let page1 = document.getElementById("page01")
 let page2 = document.getElementById("page02")
 let boxMensagem = document.getElementById("boxMensagem")
 let offset = document.getElementById("offset")
+let result = document.getElementById("result")
+
+strat.onclick = function(){
+    page1.style.display = "none"
+    page2.style.display = "flex"
+
+}
+
+voltar.onclick = function(){
+    page1.style.display = "flex"
+    page2.style.display = "none"
+
+}
+
 
 cifrar.onclick = function(){
-    document.getElementById("cifrar").value;
-    document.getElementById("boxMensagem").innerHTML = cipher.encode(offset.value, boxMensagem.value)
+    if (boxMensagem == "" || offset.value== "") {
+        alert("Verifique se inseriu a mensagem e o valor de deslocamento");
+
+    } else {
+        document.getElementById("result").innerHTML = cipher.encode (offset.value, boxMensagem.value);
+        result.style.display = "block"
+    }
+}
+descifrar.onclick = function(){
+    if (boxMensagem == "" || offset.value == "") {
+        alert("Verifique se inseriu a mensagem e o valor de deslocamento");
+        
+
+    } else {
+        document.getElementById("result").innerHTML = cipher.decode (offset.value, boxMensagem.value);
+        result.style.display = "block"
+        
+    }
 }
 console.log(cipher);
